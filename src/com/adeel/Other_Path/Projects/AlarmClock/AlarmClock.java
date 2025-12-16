de5de810481b9d8e2ab7cd364744dfc92465026f
Adeel Iqbal <iadeel26@gmail.com>
2025-12-16 10:06:16 -0800

    Project: Alarm Clock

---
+++ src/com/adeel/Other_Path/Projects/AlarmClock/AlarmClock.java
@@ -0,0 +1,82 @@
+package com.adeel.Other_Path.Projects.AlarmClock;
+
+import javax.sound.sampled.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class AlarmClock implements Runnable{
+
+    private final LocalTime alarmTime;
+    private final String filePath;
+    private final Scanner scanner;
+
+    // Constructor
+    AlarmClock(LocalTime alarmTime, String filePath, Scanner scanner) {
+        this.alarmTime = alarmTime;
+        this.filePath = filePath;
+        this.scanner = scanner;
+    }
+
+    @Override
+    public void run() {
+
+        while (LocalTime.now().isBefore(alarmTime)) {
+
+            try {
+                Thread.sleep(1000);
+
+                LocalTime now = LocalTime.now();
+
+                // int hours = LocalTime.now().getHour();
+                // int minutes = LocalTime.now().getMinute();
+                // int seconds = LocalTime.now().getSecond();
+                // System.out.printf("\r%02d:%02d:%02d", hours, minutes, seconds);
+
+                System.out.printf("\r%02d:%02d:%02d", now.getHour(), now.getMinute(), now.getSecond());
+
+
+            }
+            catch (InterruptedException e) {
+                System.out.println("Thread was interrupted");
+            }
+
+        }
+
+
+        System.out.println("\n*ALARM NOISES*");
+        playSound(filePath);
+
+    }
+
+    private void playSound(String filePath) {
+
+        File audioFile = new File(filePath);
+
+        try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile)) {
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+
+            System.out.println("Press *Enter* to stop the alarm: ");
+            scanner.nextLine();
+            clip.stop();
+
+            scanner.close();
+
+        }
+        catch (UnsupportedAudioFileException e) {
+            System.out.println("Audio file format is not supported");
+        }
+        catch (LineUnavailableException e) {
+            System.out.println("Audio is unavailable");
+        }
+        catch (IOException e) {
+            System.out.println("Error reading audio file");
+        }
+
+    }
+
+}
---
+++ src/com/adeel/Other_Path/Projects/AlarmClock/Main.java
@@ -0,0 +1,41 @@
+package com.adeel.Other_Path.Projects.AlarmClock;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // JAVA ALARM CLOCK
+
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+
+
+        String filePath = "src/com/adeel/Other_Path/Projects/AlarmClock/retro-game-alarm.wav";
+
+        while (alarmTime == null) {
+
+            try {
+
+                System.out.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = scanner.nextLine();
+
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+
+            } catch (Exception e) {
+                System.out.println("Invalid format. Please use HH:MM:SS");
+            }
+
+        }
+
+        AlarmClock alarmClock = new AlarmClock(alarmTime, filePath, scanner);
+        Thread alaramThread = new Thread(alarmClock);
+        alaramThread.start();
+        
+
+    }
+}
