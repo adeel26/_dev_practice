f8cc1a3d3986bcb45b5eb6fb52e1a82bb88000b0
Adeel Iqbal <iadeel26@gmail.com>
2026-01-21 10:34:40 -0800

    Basic: Linear Search: Even digits

--- src/com/adeel/basic/LinearSearch/EvenDigits.java
+++ src/com/adeel/basic/LinearSearch/EvenDigits.java
@@ -30,6 +30,10 @@ public class EvenDigits {
 
     // count number of digits in a number
     static int digits(int num) {
+        if (num < 0) {
+            num = num * -1;
+        }
+
         int count = 0;
 
         while (num > 0) {
