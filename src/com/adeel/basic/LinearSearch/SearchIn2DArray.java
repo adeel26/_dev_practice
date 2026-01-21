9813246fcd63afcd83c8858698c48d8cd982d64d
Adeel Iqbal <iadeel26@gmail.com>
2026-01-21 09:40:17 -0800

    Basic: Linear Search: Search in 2D Array

--- src/com/adeel/basic/LinearSearch/SearchIn2DArray.java
+++ src/com/adeel/basic/LinearSearch/SearchIn2DArray.java
@@ -14,6 +14,7 @@ public class SearchIn2DArray {
         int target = 34;
 
         System.out.println(Arrays.toString(search(arr, target)));
+        System.out.println(findMax(arr));
     }
 
     static int[] search(int[][] arr, int target) {
@@ -25,5 +26,18 @@ public class SearchIn2DArray {
             }
         }
         return new int[] {};
+    }
+
+    static int findMax(int[][] arr) {
+        int max = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr[i].length; j++) {
+                if( arr[i][j] > max) {
+                    max = arr[i][j];
+                }
+            }
+        }
+
+        return max;
     }
 }
