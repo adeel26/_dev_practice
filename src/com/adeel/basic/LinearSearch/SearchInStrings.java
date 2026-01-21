281fefcd6ea7c7b721b7a32be201ddfb2d56a093
Adeel Iqbal <iadeel26@gmail.com>
2026-01-21 05:51:13 -0800

    Basic: Linear Search: Search in Strings

---
+++ src/com/adeel/basic/LinearSearch/SearchInStrings.java
@@ -0,0 +1,37 @@
+package com.adeel.basic.LinearSearch;
+
+public class SearchInStrings {
+    public static void main(String[] args) {
+        String name = "Adeel";
+        char target = 'd';
+
+        System.out.println(search1(name, target));
+        System.out.println(search2(name, target));
+    }
+
+    static boolean search1(String str, char target) {
+        if( str.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if(target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean search2(String str, char target) {
+        if( str.length() == 0) {
+            return false;
+        }
+
+        for (char ch : str.toCharArray()){
+            if (target == ch){
+                return true;
+            }
+        }
+        return false;
+    }
+}
