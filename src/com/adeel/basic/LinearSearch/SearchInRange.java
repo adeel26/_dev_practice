e73b6d8a60dcb69790a59cf2cd0dd056df4c7142
Adeel Iqbal <iadeel26@gmail.com>
2026-01-21 06:02:19 -0800

    Basic: Linear Search: Search in Range

---
+++ src/com/adeel/basic/LinearSearch/SearchInRange.java
@@ -0,0 +1,25 @@
+package com.adeel.basic.LinearSearch;
+
+public class SearchInRange {
+    public static void main(String[] args) {
+        int[] nums = {23, 18, 17, 15, 11, 29, 33, 29};
+        int target = 15;
+
+        System.out.println(linearSearch(nums, target, 1, 4));
+    }
+
+    static int linearSearch(int[] nums, int target, int start, int end) {
+        if (nums.length == 0){
+            return -1;
+        }
+
+        for (int i = start; i <= end; i++){
+            int element = nums[i];
+            if(element == target){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
