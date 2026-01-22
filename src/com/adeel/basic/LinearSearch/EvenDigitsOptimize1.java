c3c96bc39f4e743b4d699437ec90c0e759dd17a9
Adeel Iqbal <iadeel26@gmail.com>
2026-01-21 22:04:50 -0800

    Basic: Linear Search: Even digits optimize 1

---
+++ src/com/adeel/basic/LinearSearch/EvenDigitsOptimize1.java
@@ -0,0 +1,26 @@
+package com.adeel.basic.LinearSearch;
+
+public class EvenDigitsOptimize1 {
+    public static void main(String[] args) {
+        int[] nums = {12, 2, 346, 2355, 333, 31};
+        System.out.println(findNumbers(nums));
+    }
+
+    static int findNumbers(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                continue; // 0 has 1 digit (odd) // Math.log10(0) → undefined (−Infinity) // Edge case
+            }
+
+            int digits = (int) Math.log10(Math.abs(nums[i])) + 1;
+
+            if(digits % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
