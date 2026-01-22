0b59c36fb2c2d5b701ae2e5b43a1c008cce0b74c
Adeel Iqbal <iadeel26@gmail.com>
2026-01-21 23:00:56 -0800

    Basic: Linear Search: Maximum Wealth

---
+++ src/com/adeel/basic/LinearSearch/MaxWealth.java
@@ -0,0 +1,30 @@
+package com.adeel.basic.LinearSearch;
+// https://leetcode.com/problems/richest-customer-wealth/description/
+public class MaxWealth {
+    public static void main(String[] args) {
+        int[][] accounts = {{1,5},{7,3},{3,5}};
+        System.out.println(maximumWealth(accounts));
+    }
+
+    static int maximumWealth(int[][] accounts) {
+
+        // edge case
+        if (accounts == null || accounts.length == 0) {
+            return 0;
+        }
+
+
+        int maxSum = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum = sum + accounts[i][j];
+            }
+            if(sum > maxSum){
+                maxSum = sum;
+            }
+        }
+        return maxSum;
+    }
+}
