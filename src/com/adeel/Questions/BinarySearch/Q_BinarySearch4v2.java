c706d306cea8cbfe50f6df79b7ae33756534920e
Adeel Iqbal <iadeel26@gmail.com>
2026-01-26 22:56:36 -0800

    Questions: Binary Search: Find First and Last Position of Element in Sorted Array

---
+++ src/com/adeel/Questions/BinarySearch/Q_BinarySearch4v2.java
@@ -0,0 +1,78 @@
+package com.adeel.Questions.BinarySearch;
+
+import java.util.Arrays;
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
+
+public class Q_BinarySearch4v2 {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+
+        System.out.println(Arrays.toString(searchRange(nums, target)));
+    }
+
+    static int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findStartingIndex(nums, target);
+        result[1] = findEndingIndex(nums, target);
+
+        return result;
+    }
+
+    static int findStartingIndex(int[] nums, int target) {
+        int index = -1;
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(target > nums[mid]) {
+                start = mid + 1;
+            }
+            else if (target < nums[mid]) {
+                end = mid - 1;
+            }
+            else if (target == nums[mid]) {
+                end = mid - 1;
+            }
+
+            if (target == nums[mid]) {
+                index = mid;
+            }
+
+        }
+
+        return index;
+    }
+
+    static int findEndingIndex(int[] nums, int target) {
+        int index = -1;
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(target > nums[mid]) {
+                start = mid + 1;
+            }
+            else if (target < nums[mid]) {
+                end = mid - 1;
+            }
+            else if (target == nums[mid]) {
+                start = mid + 1;
+            }
+
+            if (target == nums[mid]) {
+                index = mid;
+            }
+
+        }
+
+        return index;
+    }
+
+}
