27d4648b26ecc192048ebd655d260fffce32854c
Adeel Iqbal <iadeel26@gmail.com>
2026-01-27 21:44:26 -0800

    Questions: Binary Search: Find First and Last Position of Element in Sorted Array v2

--- src/com/adeel/Questions/BinarySearch/Q_BinarySearch4v2.java
+++ src/com/adeel/Questions/BinarySearch/Q_BinarySearch4v2.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Q_BinarySearch4v2 {
     public static void main(String[] args) {
         int[] nums = {5,7,7,8,8,10};
-        int target = 8;
+        int target = 7;
 
         System.out.println(Arrays.toString(searchRange(nums, target)));
     }
