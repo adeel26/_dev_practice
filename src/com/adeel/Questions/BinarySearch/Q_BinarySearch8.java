1b8dc73c4d864b7ddbdeb3c54a3aa23a3f388306
Adeel Iqbal <iadeel26@gmail.com>
2026-02-03 20:41:27 -0800

    Questions: Binary Search: Search in rotated sorted array

---
+++ src/com/adeel/Questions/BinarySearch/Q_BinarySearch8.java
@@ -0,0 +1,16 @@
+package com.adeel.Questions.BinarySearch;
+//https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+public class Q_BinarySearch8 {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+
+        System.out.println(search(nums, target));
+    }
+
+    static int search(int[] nums, int target) {
+
+    }
+
+}
