3f6c6fd4e1825b5d71fc4244f7bbe69e9dc6948f
Adeel Iqbal <iadeel26@gmail.com>
2026-02-08 09:56:43 -0800

    Questions: Binary Search: Find the Rotation Count in Rotated Sorted Array

--- src/com/adeel/Questions/BinarySearch/Q_BinarySearch9.java
+++ src/com/adeel/Questions/BinarySearch/Q_BinarySearch9.java
@@ -2,10 +2,7 @@ package com.adeel.Questions.BinarySearch;
 
 public class Q_BinarySearch9 {
     public static void main(String[] args) {
-        int[] nums = {4,5,6,7,0,1,2};
-        int target = 0;
 
-        System.out.println(search(nums, target));
     }
 
 
