a74e3a999b34f1088cef0f7ad6c9ee4d8ecfc8bd
Adeel Iqbal <iadeel26@gmail.com>
2026-02-21 09:01:59 -0800

    Questions: Cyclic Sort: Find the Duplicate Number Second Approach

---
+++ src/com/adeel/Questions/CyclicSort/Q_CyclicSort3b.java
@@ -0,0 +1,37 @@
+package com.adeel.Questions.CyclicSort;
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+public class Q_CyclicSort3b {
+    public static void main(String[] args) {
+        int[] nums = {1,3,4,2,2};
+        System.out.println(findDuplicate(nums));
+    }
+
+    static int findDuplicate(int[] nums) {
+        int i = 0;
+        while (i < nums.length){
+
+            if(nums[i] != i+1) {
+
+                int correctIndex = nums[i] - 1;
+                if (nums[i] != nums[correctIndex]) {
+                    swap(nums, i, correctIndex);
+                } else {
+                    return nums[i];
+                }
+
+            }else {
+                i++;
+            }
+
+        }
+
+        return -1;
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
