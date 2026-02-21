481115704705d710e49225dbe1d12d7739070c94
Adeel Iqbal <iadeel26@gmail.com>
2026-02-21 09:29:46 -0800

    Questions: Array: Concatenation of Array Optimize function

--- src/com/adeel/Questions/Array/Q_Array2.java
+++ src/com/adeel/Questions/Array/Q_Array2.java
@@ -5,16 +5,27 @@ import java.util.Arrays;
 public class Q_Array2 {
     public static void main(String[] args) {
         int[] number = {1, 2, 1};
-        System.out.println(Arrays.toString(getConcatenation(number)));
+        System.out.println(Arrays.toString(getConcatenation1(number)));
     }
 
-    static int[] getConcatenation(int[] nums) {
+    static int[] getConcatenation1(int[] nums) {
         int[] ans = new int[nums.length*2];
         for (int i = 0; i < nums.length; i++) {
             ans[i] = nums[i];
         }
         for (int i = 0; i < nums.length; i++) {
             ans[nums.length + i] = nums[i];
+        }
+        return ans;
+    }
+
+    static int[] getConcatenation2(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n * 2];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
         }
         return ans;
     }
