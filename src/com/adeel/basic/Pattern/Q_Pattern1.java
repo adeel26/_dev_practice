eb0f0ce3766a4114b4c8a6188807fae63fed674f
Adeel Iqbal <iadeel26@gmail.com>
2026-02-26 17:02:16 -0800

    Pattern: Question 1

---
+++ src/com/adeel/basic/Pattern/Q_Pattern1.java
@@ -0,0 +1,31 @@
+package com.adeel.basic.Pattern;
+
+public class Q_Pattern1 {
+    public static void main(String[] args) {
+        pattern1(5);
+    }
+
+    static void pattern1(int n) {
+
+        for (int row = 1; row <= n; row++) {
+            // for every row, run the col
+            for (int col = 1; col <= n; col++) {
+                System.out.print("*");
+            }
+            // when one row is printed, we need to add a newline
+            System.out.println();
+        }
+
+    }
+}
+
+/*
+Pattern 1
+
+    *****
+    *****
+    *****
+    *****
+    *****
+
+ */
