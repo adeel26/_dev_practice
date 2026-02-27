9bdb84cbd19e0a3d0c85fd2f37aee5a852edb79e
Adeel Iqbal <iadeel26@gmail.com>
2026-02-27 03:18:31 -0800

    Pattern: Question 4

---
+++ src/com/adeel/basic/Pattern/Q_Pattern4.java
@@ -0,0 +1,27 @@
+package com.adeel.basic.Pattern;
+
+public class Q_Pattern4 {
+    public static void main(String[] args) {
+        pattern4(5);
+    }
+
+    static void pattern4(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row ; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Pattern 4
+
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
+
+ */
