885e17065a8b94752dd3c212c235270c59ce505b
Adeel Iqbal <iadeel26@gmail.com>
2026-02-26 17:37:19 -0800

    Pattern: Question 3

--- src/com/adeel/basic/Pattern/Q_Pattern3.java
+++ src/com/adeel/basic/Pattern/Q_Pattern3.java
@@ -2,12 +2,22 @@ package com.adeel.basic.Pattern;
 
 public class Q_Pattern3 {
     public static void main(String[] args) {
-        pattern3(5);
+        pattern3a(5);
+        pattern3b(5);
     }
 
-    static void pattern3(int n) {
+    static void pattern3a(int n) {
         for (int row = 1; row <= n; row++) {
             for (int col = n; col >= row; col--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3b(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n+1-row; col++) {
                 System.out.print("*");
             }
             System.out.println();
