65eaa39d971dab91def80741c173c8d4992a6ba9
Adeel Iqbal <iadeel26@gmail.com>
2026-03-18 03:16:15 -0700

    DSA: Recursion: Concept

---
+++ src/com/adeel/DSA/Recursion/Concept.java
@@ -0,0 +1,42 @@
+package com.adeel.DSA.Recursion;
+
+public class Concept {
+    public static void main(String[] args) {
+        // write a function that takes in a number and prints it
+        // print first 5 numbers: 1 2 3 4 5
+
+        print1(1);
+    }
+
+    static void print1(int n) {
+        System.out.println(n);
+        print2(2);
+    }
+
+    static void print2(int n) {
+        System.out.println(n);
+        print3(3);
+    }
+
+    static void print3(int n) {
+        System.out.println(n);
+        print4(4);
+    }
+
+    static void print4(int n) {
+        System.out.println(n);
+        print5(5);
+    }
+
+    static void print5(int n) {
+        System.out.println(n);
+    }
+}
+
+/*
+
+-> While the function is not finished execution, it will remain in stack
+-> When a function finishes executing, it is removed from stack and the flow of program is
+   restored to where that function is called.
+
+ */
