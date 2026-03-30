acdd4b07db63a79dda4e85bfa2f5ba53f84347a3
Adeel Iqbal <iadeel26@gmail.com>
2026-03-30 11:29:24 -0700

    Questions: Recusion: Level1: Question1 N to 1

---
+++ src/com/adeel/Questions/Recursion/Level1/Q_R_Lvl1_1.java
@@ -0,0 +1,16 @@
+package com.adeel.Questions.Recursion.Level1;
+
+// N to 1
+public class Q_R_Lvl1_1 {
+    public static void main(String[] args) {
+        func(5);
+    }
+
+    static void func(int n) {
+        if(n == 0) {
+            return;
+        }
+        System.out.println(n);
+        func(n - 1);
+    }
+}
