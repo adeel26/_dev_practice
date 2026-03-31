75994cdb6159dcaf4472899712f102f95ef2f892
Adeel Iqbal <iadeel26@gmail.com>
2026-03-31 10:49:51 -0700

    Questions: Recusion: Level1: Question4 Recurisve factorial function

--- src/com/adeel/Questions/Recursion/Level1/Q_R_Lvl1_4.java
+++ src/com/adeel/Questions/Recursion/Level1/Q_R_Lvl1_4.java
@@ -1,5 +1,6 @@
 package com.adeel.Questions.Recursion.Level1;
 
+// Recursive factorial function
 public class Q_R_Lvl1_4 {
     public static void main(String[] args) {
         System.out.println(factorialRecursive(5));
