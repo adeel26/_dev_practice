ba0930b5079d079eeed1eb58e1df6cffd886b33f
Adeel Iqbal <iadeel26@gmail.com>
2026-04-06 11:52:52 -0700

    Adv: Tight coupling

---
+++ src/com/adeel/Advanced/Coupling/TightCoupling/UserDatabase.java
@@ -0,0 +1,8 @@
+package com.adeel.Advanced.Coupling.TightCoupling;
+
+public class UserDatabase {
+    public String getUserDetails() {
+        // Directly access database here
+        return "User details from database";
+    }
+}
