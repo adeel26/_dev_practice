4e0e0a5352d9e4719efb29a0b8a4d070198668c3
Adeel Iqbal <iadeel26@gmail.com>
2026-04-06 11:52:33 -0700

    Adv: Tight coupling

---
+++ src/com/adeel/Advanced/Coupling/TightCoupling/TightCouplingExample.java
@@ -0,0 +1,8 @@
+package com.adeel.Advanced.Coupling.TightCoupling;
+
+public class TightCouplingExample {
+    public static void main(String[] args) {
+        UserManager userManager = new UserManager();
+        System.out.println(userManager.getUserInfo());
+    }
+}
