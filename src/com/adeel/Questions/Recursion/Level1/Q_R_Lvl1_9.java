16853958d4d31d6d605e4fb76446c0a4b3c2607a
Adeel Iqbal <iadeel26@gmail.com>
2026-04-07 06:21:06 -0700

    Questions: Recursion: Level 1 Reverse Number with helper method

---
+++ src/com/adeel/Questions/Recursion/Level1/Q_R_Lvl1_9.java
@@ -0,0 +1,26 @@
+package com.adeel.Questions.Recursion.Level1;
+
+// Reverse Number via pure recursive way (Helper Method)
+
+public class Q_R_Lvl1_9 {
+    public static void main(String[] args) {
+        int num = 1824;
+        int reversed = reverse(num);  // Just call reverse() - clean!
+        System.out.println("Reversed number: " + reversed);
+    }
+
+    // Public method - caller doesn't need to know 'rev'
+    public static int reverse(int num) {
+        return reverseHelper(num, 0);  // Start recursion with rev = 0
+    }
+
+    // Private helper method does the actual recursion
+    private static int reverseHelper(int num, int rev) {
+        if (num == 0) {  // Base case
+            return rev;
+        }
+        // Recursive call with updated rev
+        return reverseHelper(num / 10, rev * 10 + (num % 10));
+    }
+
+}
