e09225607d5515efa0fb3a7e42cb8e7d6508f465
Adeel Iqbal <iadeel26@gmail.com>
2026-04-08 13:10:38 -0700

    Adv: Loose Coupling updated

--- src/com/adeel/Advanced/Coupling/LooseCoupling/NewDatabaseProvider.java
+++ src/com/adeel/Advanced/Coupling/LooseCoupling/NewDatabaseProvider.java
@@ -1,6 +1,7 @@
 package com.adeel.Advanced.Coupling.LooseCoupling;
 
 public class NewDatabaseProvider implements UserDataProvider{
+
     @Override
     public String getUserDetails() {
         return "New Database";
