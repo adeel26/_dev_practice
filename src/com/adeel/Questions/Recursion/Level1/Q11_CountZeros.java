9145e64445328d20ac1541c6088cdc2883343cdc
Adeel Iqbal <iadeel26@gmail.com>
2026-04-12 23:39:41 -0700

    Question: Recursion: Level 1: Count Zeros

--- src/com/adeel/Questions/Recursion/Level1/Q11_CountZeros.java
+++ src/com/adeel/Questions/Recursion/Level1/Q11_CountZeros.java
@@ -2,6 +2,27 @@ package com.adeel.Questions.Recursion.Level1;
 
 public class Q11_CountZeros {
     public static void main(String[] args) {
-
+        System.out.println(count(100230));
     }
+
+    static int count(int num) {
+        if(num == 0) {
+            return 1;
+        }
+
+        return helper(num, 0);
+    }
+
+    private static int helper(int num, int count) {
+        if (num == 0) {
+            return count;
+        }
+
+        int digit = num % 10;
+        if(digit == 0) {
+            return helper(num/10, count+1);
+        }
+        return helper(num/10, count);
+    }
+
 }
