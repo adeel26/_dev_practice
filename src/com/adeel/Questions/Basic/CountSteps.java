994f19c6d25ba0e867f7bcb4c41f193562525a1e
Adeel Iqbal <iadeel26@gmail.com>
2026-04-14 00:01:49 -0700

    Questions: Basic: Number of Steps to Reduce a Number to Zero

--- src/com/adeel/Questions/Basic/CountSteps.java
+++ src/com/adeel/Questions/Basic/CountSteps.java
@@ -1,7 +1,25 @@
 package com.adeel.Questions.Basic;
 
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+
 public class CountSteps {
     public static void main(String[] args) {
-        
+        System.out.println(numberOfSteps(14));
     }
+
+    public static int numberOfSteps(int num) {
+            int steps = 0;
+
+            while (num != 0) {
+                if(num % 2 == 0){
+                    num = num / 2;
+                }
+                else {
+                    num = num - 1;
+                }
+                steps++;
+            }
+            return steps;
+    }
+
 }
