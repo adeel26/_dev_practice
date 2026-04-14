bf2e104584325fa703eae6fff54c0ce1a555fb41
Adeel Iqbal <iadeel26@gmail.com>
2026-04-13 23:52:57 -0700

    Questions: Recursion: Level 1: Leetcode 1342

---
+++ src/com/adeel/Questions/Recursion/Level1/Q12_LeetCode1342.java
@@ -0,0 +1,25 @@
+package com.adeel.Questions.Recursion.Level1;
+
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+
+public class Q12_LeetCode1342 {
+    public static void main(String[] args) {
+        System.out.println(numberOfSteps(14));
+    }
+
+    public static int numberOfSteps(int num) {
+        return helper(num, 0);
+    }
+
+    private static int helper(int num, int steps) {
+        if (num == 0) {
+            return steps;
+        }
+
+        if (num % 2 == 0) {
+            return helper(num/2, steps+1);
+        }
+        return helper(num-1, steps+1);
+    }
+
+}
