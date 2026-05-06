f59e88b8cc5a15a12c78900d77eb22a0687210ec
Adeel Iqbal <iadeel26@gmail.com>
2026-05-05 23:48:46 -0700

    Questions: Recursion: Array: Return Array List

--- src/com/adeel/Questions/Recursion/Array/Q4_Return_ArrayList.java
+++ src/com/adeel/Questions/Recursion/Array/Q4_Return_ArrayList.java
@@ -1,7 +1,14 @@
 package com.adeel.Questions.Recursion.Array;
 
+import java.util.ArrayList;
+
 public class Q4_Return_ArrayList {
     public static void main(String[] args) {
-        
+
     }
+
+    static ArrayList findAllIndex (int[] nums, int target, int index, ArrayList<Integer> list){
+
+    }
+    
 }
