70b3026ff12c037aad65cfcdadb4088abcef657d
Adeel Iqbal <iadeel26@gmail.com>
2026-05-10 23:52:16 -0700

    Questions: Recursion: Array: Return Array List

--- src/com/adeel/Questions/Recursion/Array/Q4_Return_ArrayList.java
+++ src/com/adeel/Questions/Recursion/Array/Q4_Return_ArrayList.java
@@ -4,12 +4,21 @@ import java.util.ArrayList;
 
 public class Q4_Return_ArrayList {
     public static void main(String[] args) {
-
+        int[] nums = {3, 2, 1, 18, 18, 9};
+        System.out.println(findAllIndex(nums, 18, 0, new ArrayList<>()));
     }
 
     static ArrayList findAllIndex (int[] nums, int target, int index, ArrayList<Integer> list){
-        if (index == nums.length)
-            
+        if (index == nums.length) {
+            return list;
+        }
+
+        if (nums[index] == target) {
+            list.add(index);
+        }
+
+        return findAllIndex(nums, target, index + 1, list);
+
     }
 
 
